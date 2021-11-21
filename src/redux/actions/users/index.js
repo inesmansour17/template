a104ca9f5e0c9544414db4751108c050eb70c2c1
haikel1997 <belhaj.fraj.haikel@gmail.com>
2021-11-21 12:42:10 -0800

    add volunteer feature v1

--- src/redux/actions/users/index.js
+++ src/redux/actions/users/index.js
@@ -12,3 +12,48 @@ export const registerCenter = (user) => async (dispatch) => {
     console.log(e);
   }
 };
+
+export const addUser = (user) => async (dispatch) => {
+  try {
+    const newUser = await api.addUser(user);
+    dispatch({
+      type: types.ADD_USER,
+      user: newUser, 
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const updateUser = (user) => async (dispatch) => {
+  try {
+    const updatedUser = await api.updateUser(user);
+    dispatch({
+      type: types.UPDATE_USER,
+      user: updatedUser,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export const deleteUser = (id) => async (dispatch) => {
+  await api.deleteUser(id);
+  dispatch({
+    type: types.DELETE_USER,
+    user: id,
+  });
+};
+
+export const getAllUsers = () => async (dispatch) => {
+  await api.getAllUsers();
+  dispatch({
+    type: types.GET_ALL_USER
+    
+  });
+};
+export const setDisplayed = (value) => ({
+  type: types.SET_DISPLAYED_USER,
+  value,
+});
+
