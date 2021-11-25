db55157357c656daaf68b96b3a53590248a99ce6
SlytherING <khalfaoui.emnaa@gmail.com>
2021-11-25 09:13:21 +0000

    login done

--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -1,27 +1,30 @@
-import * as types from "../types" 
-let user = localStorage.getItem('token'); 
-const initialState =  user ? { loggedIn: true, user, errors:false } : {loggedIn: false, errors:false };
+import * as types from "../types";
+let user = localStorage.getItem("token");
+const initialState = user
+  ? { loggedIn: true, user, errors: false }
+  : { loggedIn: false, errors: false };
 
 const auth = (state = initialState, action) => {
-
   switch (action.type) {
     case types.FETCH_USER_REQUEST:
-      return { ...state, errors: true }
+      return { ...state, errors: true };
     case types.LOGIN_SUCCESS:
       return {
         ...state,
         loggedIn: true,
         errors: false,
-        user: action.payload
+        user: action.payload,
       };
-    case types.LOGIN_FAIL:
-      return { 
-        ...state, 
-        errors: true};
-    default:
-      return state 
-    }
-}
 
+    case types.LOGIN_FAIL:
+      return {
+        ...state,
+        errors: true,
+      };
+
+    default:
+      return state;
+  }
+};
 
 export default auth;
