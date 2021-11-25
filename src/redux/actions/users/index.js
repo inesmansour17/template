370c57e5514cfd518494c5e43dff65d2086c6005
SlytherING <khalfaoui.emnaa@gmail.com>
2021-11-25 07:32:20 +0000

    fixing volunteer merge

--- src/redux/actions/users/index.js
+++ src/redux/actions/users/index.js
@@ -16,7 +16,7 @@ export const registerCenter = (user) => async (dispatch) => {
 export const addUser = (user) => async (dispatch) => {
   try {
     const newUser = await api.addUser(user);
-    console.log(newUser); //a testé demain 
+    console.log(newUser); //a testé demain
     dispatch({
       type: types.ADD_USER,
       user: newUser,
@@ -25,6 +25,49 @@ export const addUser = (user) => async (dispatch) => {
     console.log(e);
   }
 };
+
+// export const updateUser = (user) => async (dispatch) => {
+//   try {
+//     const updatedUser = await api.updateUser(user);
+//     dispatch({
+//       type: types.UPDATE_USER,
+//       user: updatedUser,
+//     });
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
+// export const deleteUser = (id) => async (dispatch) => {
+//   await api.deleteUser(id);
+//   dispatch({
+//     type: types.DELETE_USER,
+//     user: id,
+//   });
+// };
+
+export const getAllUsers = () => async (dispatch) => {
+  const list = await api.getAllUsers();
+  dispatch({
+    type: types.GET_ALL_USER,
+    user: list,
+  });
+};
+
+export const setSelectedUser = (user) => ({
+  type: types.SET_SELECTED_USER,
+  user,
+});
+
+export const setDisplayed = (value) => ({
+  type: types.SET_DISPLAYED_USER,
+  value,
+});
+
+export const setDisplayUpdate = (value) => ({
+  type: types.SET_DISPLAY_UPDATE_USER,
+  value,
+});
 
 export const updateUser = (user) => async (dispatch) => {
   try {
@@ -45,49 +88,3 @@ export const deleteUser = (id) => async (dispatch) => {
     user: id,
   });
 };
-
-export const getAllUsers = () => async (dispatch) => {
- const list = await api.getAllUsers();
-  dispatch({
-    type: types.GET_ALL_USER,
-    user: list
-  });
-};
-
-export const setSelectedUser = (user) => ({
-  type: types.SET_SELECTED_USER,
-  user,
-});
-
-export const setDisplayed = (value) => ({
-  type: types.SET_DISPLAYED_USER,
-  value,
-});
-
-
-export const setDisplayUpdate = (value) => ({
-  type: types.SET_DISPLAY_UPDATE_USER,
-  value,
-});
-
-
-export const updateUser = (user) => async (dispatch) => {
-  try {
-    const updatedUser = await api.updateUser(user);
-    dispatch({
-      type: types.UPDATE_USER,
-      user: updatedUser,
-    });
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-export const deleteUser = (id) => async (dispatch) => {  
-  await api.deleteUser(id);
-  dispatch({
-    type: types.DELETE_USER,
-    user: id,
-  });
-};
-
