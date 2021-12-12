f03563b3e1c8eea8947c84cc0bb77d7650d607e5
inesmansour17 <mansourines121@gmail.com>
2021-12-13 00:18:32 +0100

    statistique vaccination

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Layout, Progress } from "antd";
 
 import SideBar from "../SideBar/SideBar";
-
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+  import {data}from './data_test'
 const { Content } = Layout;
 
 function Admin() {
@@ -16,10 +17,40 @@ function Admin() {
               className="site-layout-background"
               style={{ padding: 24, minHeight: 360 }}
             >
-              <Progress type="circle" percent={75} />
+              {/* <Progress type="circle" percent={75} />
               <Progress type="circle" percent={70} status="exception" />
-              <Progress type="circle" percent={100} />
+              <Progress type="circle" percent={100} /> */}
+                         {/* <ResponsiveContainer width="20%" height="20%"> */}
+        <BarChart
+          width={1500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="city" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="center_capacity" fill="#8884d8" />
+          <Bar dataKey="number_vaccine" fill="#82ca9d" />
+        </BarChart>
+      {/* </ResponsiveContainer> */}
             </div>
+
+            {/* <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: 360, marginTop:12 }}
+            >
+
+            
+            </div> */}
+ 
           </Content>
         </Layout>{" "}
       </Layout>
