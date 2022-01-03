1f27dd80e88f3f540401d8eb12d172a9ef81e25a
inesmansour17 <61322570+inesmansour17@users.noreply.github.com>
2022-01-03 13:23:43 +0000

    Deploying to gh-pages from @ inesmansour17/template@eb941395897393749f4df27f3f0ae1abb90f6d49 🚀

---
+++ precache-manifest.019b4630efef2fefb98b92e159821ae4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba9bebcc5d9837e91269beacfcfa0a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "426aa89a606c0f14f6d0",
+    "url": "/static/css/2.cc91c9c6.chunk.css"
+  },
+  {
+    "revision": "cb145d642054af7d40b9",
+    "url": "/static/css/main.9c00eb13.chunk.css"
+  },
+  {
+    "revision": "426aa89a606c0f14f6d0",
+    "url": "/static/js/2.022a38e7.chunk.js"
+  },
+  {
+    "revision": "7bd863abcdbfbbb26b1a26609e6334a2",
+    "url": "/static/js/2.022a38e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89adcdb196c5ac8e5fdc",
+    "url": "/static/js/3.755ee8f3.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/3.755ee8f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb51566df061ad589458",
+    "url": "/static/js/4.72b6654b.chunk.js"
+  },
+  {
+    "revision": "b13e6ca42bd2d1b36d935af34008459e",
+    "url": "/static/js/4.72b6654b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f47a59c9403f7d653e8",
+    "url": "/static/js/5.64b012be.chunk.js"
+  },
+  {
+    "revision": "cb145d642054af7d40b9",
+    "url": "/static/js/main.da3f8bf3.chunk.js"
+  },
+  {
+    "revision": "154b433a538b890503a2",
+    "url": "/static/js/runtime-main.aeba8a42.js"
+  },
+  {
+    "revision": "b5d2a7d60c05ee6da79f3d534229fedc",
+    "url": "/static/media/box-img2.b5d2a7d6.webp"
+  },
+  {
+    "revision": "8e1b12e95d75b7646223c100979e6a52",
+    "url": "/static/media/logoTun.8e1b12e9.webp"
+  },
+  {
+    "revision": "0341c2740d42bcaae25787d6b7d58707",
+    "url": "/static/media/tal3.0341c274.webp"
+  }
+]);
